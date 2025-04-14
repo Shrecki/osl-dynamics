Fixing logprob shape again

osl_dynamics/models/hmm.py
@@ -670,9 +670,10 @@ class Model(ModelBase):
 
         if self.config.implementation == "log":
             log_B = self.get_log_likelihood(x)
-            n_states, batch_size, sequence_length = log_B.shape
+            batch_size, sequence_length, n_states = log_B.shape
             log_B = log_B.transpose(2, 0, 1).reshape(n_states, -1)
             print(f"{n_states} {batch_size} {sequence_length}")
+            print(f"Reshaped log: {log_B.shape}")
             log_Pi_0 = np.log(self.state_probs_t0 + EPS)
             gamma, xi = self.baum_welch_log(log_B, log_Pi_0, P)
         else: