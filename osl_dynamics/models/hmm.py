fdc346bc6223c7c40892fc7f674f7fff6c473ffe
Shrecki <tericarax@gmail.com>
2025-12-23 00:02:49 +0100

    Optimizing xi compute for BW runtime

--- osl_dynamics/models/hmm.py
+++ osl_dynamics/models/hmm.py
@@ -384,9 +384,9 @@ class Model(ModelBase):
             for element in dataset:
                 x = self._unpack_inputs(element)
                 # Update state probabilities
-                #start_post = time.time()
+                start_post = time.time()
                 gamma, xi_sum = self.get_posterior(x)
-                #end_post = time.time()
+                end_post = time.time()
 
                 #start_other = time.time()
                 # Update transition probability matrix
@@ -707,8 +707,8 @@ class Model(ModelBase):
             log_B = log_B.transpose(2, 0, 1).reshape(n_states, -1).T
             gamma, xi = self.baum_welch_log_optimized(log_B, Pi_0, P)
             end_bw = time.time()
-            #print(f"LL compute time: {end_ll - start_ll}")
-            #print(f"BW compute time: {end_bw - start_bw}")
+            print(f"LL compute time: {end_ll - start_ll}")
+            print(f"BW compute time: {end_bw - start_bw}")
         else:
             B = self.get_likelihood(x)            
             gamma, xi = self.baum_welch(B, Pi_0, P)
